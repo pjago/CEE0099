b1e4b85d8b763fe88dcaa1fce0df9b07655a61e0
Pedro Martins <pedro.m4rtins@gmail.com>
2017-09-05 22:27:35 -0300

    Timer1 RTC

--- main.c
+++ main.c
@@ -5,17 +5,31 @@
  * Created on September 4, 2017, 10:11 AM
  */
 
+// CONFIG
+
+#pragma config FOSC = EXTRC     // Oscillator Selection bits (RC oscillator)
+#pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled)
+#pragma config PWRTE = OFF      // Power-up Timer Enable bit (PWRT disabled)
+#pragma config BOREN = OFF      // Brown-out Reset Enable bit (BOR disabled)
+#pragma config LVP = OFF        // Low-Voltage (Single-Supply) In-Circuit Serial Programming Enable bit (RB3 is digital I/O, HV on MCLR must be used for programming)
+#pragma config CPD = OFF        // Data EEPROM Memory Code Protection bit (Data EEPROM code protection off)
+#pragma config WRT = OFF        // Flash Program Memory Write Enable bits (Write protection off; all program memory may be written to by EECON control)
+#pragma config CP = OFF         // Flash Program Memory Code Protection bit (Code protection off)
+
 #include <xc.h>
 
+const unsigned int T1FOSC = 0x1000;  // RTC = 0x8000 Hz, CKPS = 0b11 @T1CON
+const unsigned int RPSMIN = 8;       // RPS minimal pickup value @rps_timer
+
 typedef struct {
-    unsigned int time;
     volatile int count;
-    int value;
-    int div;
-    int max;
+    unsigned int time;
+    unsigned int value;
+    unsigned int div;
+    unsigned int max;
 } rpm;
 
-rpm FAN = {0, -1, 0, 7, 5000};
+rpm FAN = {-1, 0, 0, 7, 5000};
 
 void interrupt raycast_intersection_cleared (void) {
     RB7 = !RB7;
@@ -24,36 +38,31 @@ void interrupt raycast_intersection_cleared (void) {
     return;
 }
 
-rpm rpm_timer (rpm in, unsigned int t) {
-    rpm out; 
-    
-    if (out.time > t) {
-        out.count = 0;
-        out.value = in.value;
-        out.time = t;
+rpm rps_timer1 (rpm x, unsigned int t) {
+    if (x.time > t) {
+        x.count = 0;
+        x.time = t;
     }
-    else if (in.count > 0) {
-        out.count = 0;
-        out.value = 37500 / (t - in.time) * 1000 * in.count / in.div;
-        if (out.value > in.max) out.max = out.value;
-        out.time = t;
+    else if (x.count > 0) {
+        x.value = T1FOSC / (t - x.time) * x.count / x.div;
+        if (x.value > x.max) x.max = x.value;
+        x.count = 0;
+        x.time = t;
     }
-    else if (t - in.time > 37500) {
-        out.count = -1;
-        out.value = 0;
-        out.time = t;
+    else if ( (t - x.time) > (T1FOSC / RPSMIN) ) {
+        x.count = -1;
+        x.value = 0;
+        x.time = t;
     }
-    else {
-        out = in;
-    }
-    return out;
+    return x;
 }
 
 int main (void) {
-    //setup
-    TMR1CS = 0;             // TMR1 uses internal clock of 5 MHz
+    //SETUP
+
+    TMR1CS = 1;             // TMR1 uses external clock of 2^15 Hz
     T1CKPS0 = 1;            // using a pre-scaler of 8 ...
-    T1CKPS1 = 1;            // TMR1 increments at 625 kHz
+    T1CKPS1 = 1;            // TMR1 increments at 2^12 kHz
     TMR1ON = 1;             // TMR1 enabled
     
     CCP1M3 = 1;             // CCP1 module is on PWM mode ...
@@ -73,10 +82,11 @@ int main (void) {
     INTE = 1;               // enable the external interrupt
     GIE = 1;                // global interrupt enable
 
-    //loop
+    //LOOP   
+
     while (1) {
-        FAN = rpm_timer(FAN, TMR1);
-        CCPR1L = 10 * FAN.value / FAN.max * 10; //TODO: MATLAB COM
+        CCPR1L = 100 * FAN.value / FAN.max;
+        FAN = rps_timer1(FAN, TMR1);
     }
     return 0;
 }
