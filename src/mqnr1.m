178b63024f644390217427372dd4aa583f9eae6a
pjago <pedro.m4rtins@gmail.com>
2017-11-06 17:45:09 -0300

    mqnr

---
+++ src/mqnr1.m
@@ -0,0 +1,27 @@
+%Gz = mqnr(y, u, T) não recursivo
+function Gz = mqnr(y, u, T)    
+    n = size(y, 1);
+    for i = 1:size(y, 2)
+        Y=[];
+        fi=[];
+        for k=4:n 
+            y1 = y(k-1, i);
+            u1 = at(u, k-1, i);
+            Y  = [Y; y(k, i)];
+            fi = [fi; -y1 u1];   
+        end
+        teta(:, i) = (fi'*fi)\fi'*Y;
+    end;
+    
+    uxi = (1:size(u, 2))';
+    
+    for i = 1:size(y, 2)
+        a1=teta(1, i); 
+        b1=teta(2, i); 
+        
+        z = tf('z', T, 'variable', 'z^-1');
+        j = at(uxi, i);
+        Gz(i, j) = b1/(1 + a1*z^-1);
+        Gz(i, j).ioDelay = 1;
+    end
+end
