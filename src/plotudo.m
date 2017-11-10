ab111fc955d946ec0417b7f58c4201ece2c820cd
pjago <pedro.m4rtins@gmail.com>
2017-11-10 15:29:41 -0300

    corrige help do plotudo

--- src/plotudo.m
+++ src/plotudo.m
@@ -1,7 +1,7 @@
-%[fh1 fh2] = plotudo(y, r, t, e, u, usat, ...)
-%plotudo(y, r, t, e, u, fsat(u), 1)    plota -yD, eD, eI
-%plotudo(y, r, t, e, u, fsat(u), ~, 1) plota fft
-function [hf, hfu] = plotudo(t, y, r, e, u, usat, varargin)
+%[fh1 fh2] = plotudo(t, y, r, e, u, v, ...) 
+%plotudo(t, y, r, e, u, v, 1)    plota -yD, eD, eI
+%plotudo(t, y, r, e, u, v, ~, 1) plota fft
+function [hf, hfu] = plotudo(t, y, r, e, u, v, varargin)
 
     default = {0, 0};
     custom = [varargin default{(1 + length(varargin)):end}];
@@ -40,7 +40,7 @@ function [hf, hfu] = plotudo(t, y, r, e, u, usat, varargin)
         xlabel('Tempo (s)')
         if verbose
             h1 = stairs(t', at(u,    ':', k), 'k', 'LineWidth', 0.5);
-            h2 = stairs(t', at(usat, ':', k), 'ko', 'MarkerSize', 2.5, 'LineWidth', 0.5);
+            h2 = stairs(t', at(v, ':', k), 'ko', 'MarkerSize', 2.5, 'LineWidth', 0.5);
             h3 = stairs(t', at(e,    ':', k), 'LineWidth', 1, 'Color', [0 0.447 0.741]);
             h4 = stairs(t', at(eI,   ':', k), 'LineWidth', 1, 'Color', [0.9290 0.6940 0.1250]);
             h5 = stairs(t', at(eD,   ':', k), 'LineWidth', 1, 'Color', [0.850 0.325 0.098]);
@@ -49,7 +49,7 @@ function [hf, hfu] = plotudo(t, y, r, e, u, usat, varargin)
             clickableLegend([h1; h2; h3; h4; h5; h6], {'u, v', 'e', 'eI', 'eD'}, 'groups', [1 1 2 3 4 4]);
         else
             h1 = stairs(t', at(u,    ':', k), 'k', 'LineWidth', 0.5);
-            h2 = stairs(t', at(usat, ':', k), 'ko', 'MarkerSize', 2.5, 'LineWidth', 0.5);
+            h2 = stairs(t', at(v, ':', k), 'ko', 'MarkerSize', 2.5, 'LineWidth', 0.5);
             h3 = stairs(t', at(e,    ':', k), 'LineWidth', 1.5, 'Color', [0 0.447 0.741]);
             plot(t, zeros(size(t)), 'r--', 'LineWidth', 0.5);
             clickableLegend([h1; h2; h3], {'u, v', 'e'}, 'groups', [1 1 2]);
@@ -68,7 +68,7 @@ function [hf, hfu] = plotudo(t, y, r, e, u, usat, varargin)
                 h1 = plot_fft( y(      ':', k), 1/T, 'LineWidth', 1.5, 'Color', [0 0.447 0.741]);
                 h2 = plot_fft(at(r,    ':', k), 1/T, 'r--', 'LineWidth', 0.5);
                 h3 = plot_fft(at(u,    ':', k), 1/T, 'k', 'LineWidth', 0.5);
-                h4 = plot_fft(at(usat, ':', k), 1/T, 'ko', 'MarkerSize', 4, 'LineWidth', 0.5);
+                h4 = plot_fft(at(v, ':', k), 1/T, 'ko', 'MarkerSize', 4, 'LineWidth', 0.5);
                 h5 = plot_fft(at(e,    ':', k), 1/T, '*', 'MarkerSize', 4, 'Color', [0 0.447 0.741]);
                 h6 = plot_fft(at(eI,   ':', k), 1/T, '*', 'MarkerSize', 4, 'Color', [0.9290 0.6940 0.1250]);
                 h7 = plot_fft(at(eD,   ':', k), 1/T, '*', 'MarkerSize', 4, 'Color', [0.850 0.325 0.098]);
@@ -78,7 +78,7 @@ function [hf, hfu] = plotudo(t, y, r, e, u, usat, varargin)
                 h1 = plot_fft( y(      ':', k), 1/T, 'LineWidth', 1.5, 'Color', [0 0.447 0.741]);
                 h2 = plot_fft(at(r,    ':', k), 1/T, 'r--', 'LineWidth', 0.5);
                 h3 = plot_fft(at(u,    ':', k), 1/T, 'k', 'LineWidth', 0.5);
-                h4 = plot_fft(at(usat, ':', k), 1/T, 'ko', 'MarkerSize', 4, 'LineWidth', 0.5);
+                h4 = plot_fft(at(v, ':', k), 1/T, 'ko', 'MarkerSize', 4, 'LineWidth', 0.5);
                 clickableLegend([h1; h2; h3; h4], {'y', 'r', 'u, v'}, 'groups', [1 2 3 3]);
             end
             title(sprintf('Fourier %d', k))
