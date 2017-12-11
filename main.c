cdac811355c5336577fe58e94697b70f0d8a5fe8
pjago <pedro.m4rtins@gmail.com>
2017-12-11 01:27:48 -0300

    first

--- main.c
+++ main.c
@@ -7,18 +7,73 @@
 
 // CONFIG
 
-#pragma config FOSC = HS        // Oscillator Selection bits (High Speed crystal)
-#pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled)
-#pragma config PWRTE = OFF      // Power-up Timer Enable bit (PWRT disabled)
-#pragma config BOREN = OFF      // Brown-out Reset Enable bit (BOR disabled)
-#pragma config LVP = OFF        // Low-Voltage (Single-Supply) In-Circuit Serial Programming Enable bit (RB3 is digital I/O, HV on MCLR must be used for programming)
-#pragma config CPD = OFF        // Data EEPROM Memory Code Protection bit (Data EEPROM code protection off)
-#pragma config WRT = OFF        // Flash Program Memory Write Enable bits (Write protection off; all program memory may be written to by EECON control)
-#pragma config CP = OFF         // Flash Program Memory Code Protection bit (Code protection off)
+// CONFIG1L
+#pragma config PLLDIV = 5       // PLL Prescaler Selection bits (Divide by 5 (20 MHz oscillator input))
+#pragma config CPUDIV = OSC1_PLL2// System Clock Postscaler Selection bits ([Primary Oscillator Src: /1][96 MHz PLL Src: /2])
+#pragma config USBDIV = 1       // USB Clock Selection bit (used in Full-Speed USB mode only; UCFG:FSEN = 1) (USB clock source comes directly from the primary oscillator block with no postscale)
+
+// CONFIG1H
+#pragma config FOSC = HSPLL_HS  // Oscillator Selection bits (HS oscillator, PLL enabled (HSPLL))
+#pragma config FCMEN = OFF      // Fail-Safe Clock Monitor Enable bit (Fail-Safe Clock Monitor disabled)
+#pragma config IESO = OFF       // Internal/External Oscillator Switchover bit (Oscillator Switchover mode disabled)
+
+// CONFIG2L
+#pragma config PWRT = OFF       // Power-up Timer Enable bit (PWRT disabled)
+#pragma config BOR = OFF        // Brown-out Reset Enable bits (Brown-out Reset disabled in hardware and software)
+#pragma config BORV = 3         // Brown-out Reset Voltage bits (Minimum setting 2.05V)
+#pragma config VREGEN = OFF     // USB Voltage Regulator Enable bit (USB voltage regulator disabled)
+
+// CONFIG2H
+#pragma config WDT = OFF        // Watchdog Timer Enable bit (WDT disabled (control is placed on the SWDTEN bit))
+#pragma config WDTPS = 32768    // Watchdog Timer Postscale Select bits (1:32768)
+
+// CONFIG3H
+#pragma config CCP2MX = ON      // CCP2 MUX bit (CCP2 input/output is multiplexed with RC1)
+#pragma config PBADEN = ON      // PORTB A/D Enable bit (PORTB<4:0> pins are configured as analog input channels on Reset)
+#pragma config LPT1OSC = OFF    // Low-Power Timer 1 Oscillator Enable bit (Timer1 configured for higher power operation)
+#pragma config MCLRE = ON       // MCLR Pin Enable bit (MCLR pin enabled; RE3 input pin disabled)
+
+// CONFIG4L
+#pragma config STVREN = ON      // Stack Full/Underflow Reset Enable bit (Stack full/underflow will cause Reset)
+#pragma config LVP = OFF        // Single-Supply ICSP Enable bit (Single-Supply ICSP disabled)
+#pragma config ICPRT = OFF      // Dedicated In-Circuit Debug/Programming Port (ICPORT) Enable bit (ICPORT disabled)
+#pragma config XINST = OFF      // Extended Instruction Set Enable bit (Instruction set extension and Indexed Addressing mode disabled (Legacy mode))
+
+// CONFIG5L
+#pragma config CP0 = OFF        // Code Protection bit (Block 0 (000800-001FFFh) is not code-protected)
+#pragma config CP1 = OFF        // Code Protection bit (Block 1 (002000-003FFFh) is not code-protected)
+#pragma config CP2 = OFF        // Code Protection bit (Block 2 (004000-005FFFh) is not code-protected)
+#pragma config CP3 = OFF        // Code Protection bit (Block 3 (006000-007FFFh) is not code-protected)
+
+// CONFIG5H
+#pragma config CPB = OFF        // Boot Block Code Protection bit (Boot block (000000-0007FFh) is not code-protected)
+#pragma config CPD = OFF        // Data EEPROM Code Protection bit (Data EEPROM is not code-protected)
+
+// CONFIG6L
+#pragma config WRT0 = OFF       // Write Protection bit (Block 0 (000800-001FFFh) is not write-protected)
+#pragma config WRT1 = OFF       // Write Protection bit (Block 1 (002000-003FFFh) is not write-protected)
+#pragma config WRT2 = OFF       // Write Protection bit (Block 2 (004000-005FFFh) is not write-protected)
+#pragma config WRT3 = OFF       // Write Protection bit (Block 3 (006000-007FFFh) is not write-protected)
+
+// CONFIG6H
+#pragma config WRTC = OFF       // Configuration Register Write Protection bit (Configuration registers (300000-3000FFh) are not write-protected)
+#pragma config WRTB = OFF       // Boot Block Write Protection bit (Boot block (000000-0007FFh) is not write-protected)
+#pragma config WRTD = OFF       // Data EEPROM Write Protection bit (Data EEPROM is not write-protected)
+
+// CONFIG7L
+#pragma config EBTR0 = OFF      // Table Read Protection bit (Block 0 (000800-001FFFh) is not protected from table reads executed in other blocks)
+#pragma config EBTR1 = OFF      // Table Read Protection bit (Block 1 (002000-003FFFh) is not protected from table reads executed in other blocks)
+#pragma config EBTR2 = OFF      // Table Read Protection bit (Block 2 (004000-005FFFh) is not protected from table reads executed in other blocks)
+#pragma config EBTR3 = OFF      // Table Read Protection bit (Block 3 (006000-007FFFh) is not protected from table reads executed in other blocks)
+
+// CONFIG7H
+#pragma config EBTRB = OFF      // Boot Block Table Read Protection bit (Boot block (000000-0007FFh) is not protected from table reads executed in other blocks)
 
 #include <conio.h>
 #include <stdio.h>
 #include <xc.h>
+
+#define _XTAL_FREQ 20000000 // CLK = 20 Mhz
 
 const unsigned long IFOSC = 5000000;  // CLK = 20 Mhz, CLK/4 = 5 Mhz @delay
 const unsigned int T1FOSC = 360;      // T1CLK = B*T1FOSC Hz, CKPS = 0b00 @T1CON
@@ -38,10 +93,10 @@ struct {
 // UI
 
 void beep () {
-    _delay(IFOSC/10);
+    __delay_ms(100);
     for (int j = 0; j < 100; ++j) {
-        BUZ = 1; _delay(IFOSC/1000);
-        BUZ = 0; _delay(IFOSC/2000);
+        BUZ = 1; __delay_us(1000);
+        BUZ = 0; __delay_us(500);
     } 
 }
 
@@ -49,8 +104,8 @@ void putch (char msg) {
     if (PUT) {
         LCD.RS = 1;
         LCD.CH = msg;
-        LCD.EN = 1; _delay(IFOSC/1000000);
-        LCD.EN = 0; _delay(IFOSC/20000);
+        LCD.EN = 1; __delay_us(1);
+        LCD.EN = 0; __delay_us(50);
     }
     else {
         while(!TXIF) continue;
@@ -67,19 +122,19 @@ char getch () {
 void prog_lcd (char msg) {
     LCD.RS = 0;
     LCD.CH = msg;
-    LCD.EN = 1; _delay(IFOSC/1000000);
-    LCD.EN = 0; _delay(IFOSC/20000);
+    LCD.EN = 1; __delay_us(1);
+    LCD.EN = 0; __delay_us(50);
     return;
 }
 
 void init_lcd () {
     PUT = 1;
     LCD.RW = 0;
-    prog_lcd(0x30); _delay(IFOSC/200);
-    prog_lcd(0x30); _delay(IFOSC/10000);
+    prog_lcd(0x30); __delay_ms(5);
+    prog_lcd(0x30); __delay_us(100);
     prog_lcd(0x30);
     prog_lcd(0x38);
-    prog_lcd(0x01); _delay(IFOSC/500);
+    prog_lcd(0x01); __delay_ms(2);
     prog_lcd(0x0C);
     prog_lcd(0x06);
     prog_lcd(0x80);
@@ -106,9 +161,9 @@ typedef struct {
 
 rpm FAN = {-1, 0};
 
-void interrupt raycast_intersection_cleared (void) {
+void high_priority interrupt raycast_intersection_cleared (void) {
     FAN.count++;
-    INTF = 0;
+    INT0IF = 0;
     return;
 }
 
@@ -180,7 +235,9 @@ int main (void) {
     PR2 = 100;              // PWM period = [(PR2)+1]*4*TOSC*T2CKPS
     T2CON = 0x04;           // TMR2 enabled at maximum frequency
     TRISC = 0xFB;           // C2 powers a LED
-    OPTION_REG = 0x38;      // B pull-ups, B0 falling, T0CKI falling, TMR0/1
+    T0CON = 0xFF;           // T0CKI falling, TMR0/1
+    INTCON = 0x90;          // GIEH enabled, INT0IE enabled
+    INTCON2 = 0x00;         // B pull-ups, B0 falling
     TRISB = 0x7F;           // B0 reads IR, B7 powers a LED
     INTCON = 0x90;          // enable external interrupt on B0
     TRISD = 0x00;           // LCD output
