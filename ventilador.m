0c791c203a33247f15023c5424398b3e8d488915
Pedro Martins <pedro.m4rtins@gmail.com>
2017-12-15 15:18:48 -0300

    simulink

---
+++ ventilador.m
@@ -0,0 +1,141 @@
+function ventilador(block)
+  
+%MSFUNTMPL_BASIC A Template for a Level-2 MATLAB S-Function
+%   The MATLAB S-function is written as a MATLAB function with the
+%   same name as the S-function. Replace 'msfuntmpl_basic' with the 
+%   name of your S-function.
+%
+%   It should be noted that the MATLAB S-function is very similar
+%   to Level-2 C-Mex S-functions. You should be able to get more
+%   information for each of the block methods by referring to the
+%   documentation for C-Mex S-functions.
+%
+%   Copyright 2003-2010 The MathWorks, Inc.
+
+%%
+%% The setup method is used to set up the basic attributes of the
+%% S-function such as ports, parameters, etc. Do not add any other
+%% calls to the main body of the function.
+%%
+addpath('src'); % add readlino and writelino functions
+setup(block);
+
+%endfunction
+
+%% Function: setup ===================================================
+%% Abstract:
+%%   Set up the basic characteristics of the S-function block such as:
+%%   - Input ports
+%%   - Output ports
+%%   - Dialog parameters
+%%   - Options
+%%
+%%   Required         : Yes
+%%   C-Mex counterpart: mdlInitializeSizes
+%%
+function setup(block)
+% Register number of ports
+block.NumInputPorts  = 1;
+block.NumOutputPorts = 1;
+
+% Setup port properties to be inherited or dynamic
+block.SetPreCompInpPortInfoToDynamic;
+block.SetPreCompOutPortInfoToDynamic;
+
+% Override input port properties
+block.InputPort(1).Dimensions        = 1;
+block.InputPort(1).DatatypeID  = 0;  % double
+block.InputPort(1).Complexity  = 'Real';
+block.InputPort(1).DirectFeedthrough = true;
+
+% Override output port properties
+block.OutputPort(1).Dimensions       = 1;
+block.OutputPort(1).DatatypeID  = 0; % double
+block.OutputPort(1).Complexity  = 'Real';
+
+% Register parameters
+block.NumDialogPrms     = 1;
+
+% Register sample times
+%  [0 offset]            : Continuous sample time
+%  [positive_num offset] : Discrete sample time
+%
+%  [-1, 0]               : Inherited sample time
+%  [-2, 0]               : Variable sample time
+block.SampleTimes = [-1 0];
+
+% Specify the block simStateCompliance. The allowed values are:
+%    'UnknownSimState', < The default setting; warn and assume DefaultSimState
+%    'DefaultSimState', < Same sim state as a built-in block
+%    'HasNoSimState',   < No sim state
+%    'CustomSimState',  < Has GetSimState and SetSimState methods
+%    'DisallowSimState' < Error out when saving or restoring the model sim state
+block.SimStateCompliance = 'DefaultSimState';
+
+%% -----------------------------------------------------------------
+%% The MATLAB S-function uses an internal registry for all
+%% block methods. You should register all relevant methods
+%% (optional and required) as illustrated below. You may choose
+%% any suitable name for the methods and implement these methods
+%% as local functions within the same file. See comments
+%% provided for each function for more information.
+%% -----------------------------------------------------------------
+
+block.RegBlockMethod('Start', @Start);
+block.RegBlockMethod('Outputs', @Outputs);     % Required
+block.RegBlockMethod('Terminate', @Terminate); % Required
+
+%end setup
+
+%%
+%% Start:
+%%   Functionality    : Called once at start of model execution. If you
+%%                      have states that should be initialized once, this 
+%%                      is the place to do it.
+%%   Required         : No
+%%   C-MEX counterpart: mdlStart
+%%
+function Start(block)
+COM = block.DialogPrm(1).Data; % get the COM port number
+T = block.InputPort(1).SampleTime; % get sampling time
+s = serial(COM,'BaudRate',19200); % create the serial handler
+s.terminator = 'LF';    
+out = instrfind(s);
+if strcmp(out.status, 'closed')
+  fclose(instrfind);
+  fopen(s);
+end
+s.Timeout = T/2;
+set_param(block, 'UserData', s);
+
+%end Start
+
+%%
+%% Outputs:
+%%   Functionality    : Called to generate block outputs in
+%%                      simulation step
+%%   Required         : Yes
+%%   C-MEX counterpart: mdlOutputs
+%%
+function Outputs(block)
+T = block.InputPort(1).SampleTime; % get sampling time
+s = get_param(block.BlockHandle, 'UserData'); % get serial handler
+writelino(s, block.InputPort(1)); % write to plant
+block.OutputPort(1).Data = readlino(s, T); % read from plant
+
+%end Outputs
+
+%%
+%% Terminate:
+%%   Functionality    : Called at the end of simulation for cleanup
+%%   Required         : Yes
+%%   C-MEX counterpart: mdlTerminate
+%%
+function Terminate(block)
+s = get_param(block.BlockHandle, 'UserData'); % get serial handler
+flushinput(s); % todo: write stopino function? 
+flushoutput(s);
+fwrite(s, '2', 'uint8');
+fwrite(s, value, 'uint8');
+
+%end Terminate
